dd4dd01243d51dbabc0151511c089d5d6e8fa6a9
Valentin Zirimwabagabo <143815773+vzirimwabagabo@users.noreply.github.com>
2025-08-04 21:54:47 +0300

    Update config.py to use Railway MySQL env vars and add .gitignore

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ db_config = {
     'host': os.environ.get('DB_HOST', 'localhost'),
     'user': os.environ.get('DB_USER', 'root'),
     'password': os.environ.get('DB_PASSWORD', ''),
-    'database': os.environ.get('DB_NAME', 'CardVault')
+    'database': os.environ.get('DB_NAME', 'CardVault'),
+    'port': int(os.environ.get('DB_PORT', 3306))
 }
