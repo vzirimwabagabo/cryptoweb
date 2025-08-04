a2d246eba8a000e1b743fb5990ddc6903c647729
Valentin Zirimwabagabo <143815773+vzirimwabagabo@users.noreply.github.com>
2025-08-04 20:36:09 +0300

    Refactor config.py to use environment variables for DB settings

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-# config.py
+import os
+
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'CardVault'
+    'host': os.environ.get('DB_HOST', 'localhost'),
+    'user': os.environ.get('DB_USER', 'root'),
+    'password': os.environ.get('DB_PASSWORD', ''),
+    'database': os.environ.get('DB_NAME', 'CardVault')
 }
